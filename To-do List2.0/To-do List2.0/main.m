3a4d4af20821f37da093e7b580854550b11def5a
LXH <longxiaohua90@163.com>
2016-07-13 15:59:21 +0800

    To-do List2.0

---
+++ To-do List2.0/To-do List2.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  To-do List2.0
+//
+//  Created by lxh_miao on 16/7/9.
+//  Copyright © 2016年 lxh_miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
